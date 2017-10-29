b1a29c32fc91066410bae36f9b9149a9da9cf098
Yilong <yhua4993@uni.sydney.edu.au>
2017-10-29 18:43:22 +1100

    - Redux Trial No.1 A not appropriate trial with simplest basic usage of redux 一次不太妥当的redux的最简单的用法 issue: no store.subscribe() is successfully used for UI update 不能成功刷新UI

--- src/Routes/Products/containers/Products.jsx
+++ src/Routes/Products/containers/Products.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ButtonLink from '../../../components/ButtonLink';
+import Button from '../../../components/Button';
 
-const Products = () => (
+const Products = (props) => (
   <div style={{
     width: '90%', margin: '20px auto',
     }}
   >
     <div style={{ textAlign: 'right' }}>
       <ButtonLink link="/products/new">+ new product</ButtonLink>
+    </div>
+    <div style={{ textAlign: 'center', paddingTop: 50 }}>
+      <div style={{ textAlign: 'center', padding: 50 }}>
+        counter : {props.value ? props.value : 'NONE'}
+      </div>
+      <Button func={props.onIncrement}>increase</Button>
     </div>
   </div>
 );
--- src/Routes/Products/index.jsx
+++ src/Routes/Products/index.jsx
@@ -1,3 +1,19 @@
+import React from 'react';
+import { createStore } from 'redux';
 import Products from './containers/Products';
+import reducer from './modules/products';
 
-export default Products;
+const initialState = 65;
+const store = createStore(reducer, initialState);
+
+const ProductsWithReduxStore = () => {
+  return (
+    <Products
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: 'COUNTER_INCREMENT', payload: 1 })} // action 直接写在了这里
+    />
+  );
+};
+
+export default ProductsWithReduxStore;
+
