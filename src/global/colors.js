6666d31d56acd9efdba3656373d5c217ddb9fa66
Yilong <yhua4993@uni.sydney.edu.au>
2017-11-02 17:56:17 +1100

    - add: colors

--- src/global/colors.js
+++ src/global/colors.js
@@ -1,2 +1,67 @@
-export const blue1 = '#64B5F6';
+// ----- material blue ------------
+export const blue50 = '#E3F2FD';
+export const blue100 = '#BBDEFB';
+export const blue200 = '#90CAF9';
+export const blue300 = '#64B5F6';
+export const blue400 = '#42A5F5';
+export const blue500 = '#2196F3';
+export const blue600 = '#1E88E5';
+export const blue700 = '#1976D2';
+export const blue800 = '#1565C0';
+export const blue900 = '#0D47A';
+export const blueA100 = '#82B1FF';
+export const blueA200 = '#448AFF';
+export const blueA400 = '#2979FF';
+export const blueA700 = '#2962FF';
+
+// ----- material cyan ------------
+
+export const cyan50 = '#E0F7FA';
+export const cyan100 = '#B2EBF2';
+export const cyan200 = '#80DEEA';
+export const cyan300 = '#4DD0E1';
+export const cyan400 = '#26C6DA';
+export const cyan500 = '#00BCD4';
+export const cyan600 = '#00ACC1';
+export const cyan700 = '#0097A7';
+export const cyan800 = '#00838F';
+export const cyan900 = '#006064';
+export const cyanA100 = '#84FFFF';
+export const cyanA200 = '#18FFFF';
+export const cyanA400 = '#00E5FF';
+export const cyanA700 = '#00B8D4';
+
+
+// ----- material pink ------------
+export const pink50 = '#FCE4EC';
+export const pink100 = '#F8BBD0';
+export const pink200 = '#F48FB1';
+export const pink300 = '#F06292';
+export const pink400 = '#EC407A';
+export const pink500 = '#E91E63';
+export const pink600 = '#D81B60';
+export const pink700 = '#C2185B';
+export const pink800 = '#AD1457';
+export const pink900 = '#880E4F';
+export const pinkA100 = '#FF80AB';
+export const pinkA200 = '#FF4081';
+export const pinkA400 = '#F50057';
+export const pinkA700 = '#C51162';
+
+// ----- material teal ------------
+export const teal50 = '#E0F2F1';
+export const teal100 = '#B2DFDB';
+export const teal200 = '#80CBC4';
+export const teal300 = '#4DB6AC';
+export const teal400 = '#26A69A';
+export const teal500 = '#009688';
+export const teal600 = '#00897B';
+export const teal700 = '#00796B';
+export const teal800 = '#00695C';
+export const teal900 = '#004D40';
+export const tealA100 = '#A7FFEB';
+export const tealA200 = '#64FFDA';
+export const tealA400 = '#1DE9B6';
+export const tealA700 = '#00BFA5';
+
 export const white = '#fff';
