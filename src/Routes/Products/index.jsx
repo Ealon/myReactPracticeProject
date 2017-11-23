0afcd80c28599adfef687351808c2e950711815a
Yilong <yhua4993@uni.sydney.edu.au>
2017-11-23 14:42:29 +1100

    - delete: removed Redux related codes

--- src/Routes/Products/index.jsx
+++ src/Routes/Products/index.jsx
@@ -1,19 +1,3 @@
-import React from 'react';
-import { createStore } from 'redux';
 import Products from './containers/Products';
-import reducer from './modules/products';
 
-const initialState = 65;
-const store = createStore(reducer, initialState);
-
-const ProductsWithReduxStore = () => {
-  return (
-    <Products
-      value={store.getState()}
-      onIncrement={() => store.dispatch({ type: 'COUNTER_INCREMENT', payload: 1 })} // action 直接写在了这里
-    />
-  );
-};
-
-export default ProductsWithReduxStore;
-
+export default Products;
